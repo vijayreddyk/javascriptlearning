bae7e1acd27761c37a3be3e4a5a3868cf0dcda89
kvijay452@gmail.com <kvijay452@gmail.com>
2016-08-31 22:58:29 +0530

    validation of a form

---
+++ treemaps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class treemaps{
+	static  int a=9999;
+	public static void main(String args[]){
+		TreeMap<String,String> tr=new TreeMap<String,String>();
+		tr.put("hello","hi");
+		a=1111;
+		System.out.println(tr+" "+a);
+	}
+}
